95eef80e86dab86ba345fa5589875c21e9d340a0
mateo vukusic <mateo.vukusic@krokodil.hr>
2020-03-29 20:10:46 +0200

    Add generators

---
+++ lib/generators/custom_scope/install_generator.rb
@@ -0,0 +1,20 @@
+module CustomScope
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../templates', __FILE__)
+
+      def copy_migration
+        timestamp = Time.now.utc.strftime('%Y%m%d%H%M%S').to_i
+
+        migration_files = [{
+          new_file_name: 'create_default_scope', orgin_file_name: 'migration'
+        }]
+
+        migration_files.each do |migration_file|
+          copy_file "#{migration_file[:new_file_name]}.rb", "db/migrate/#{timestamp}_#{migration_file[:new_file_name]}.rb"
+          timestamp += 1
+        end
+      end
+    end
+  end
+end
